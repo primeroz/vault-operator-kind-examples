33049facbcf312ec5b1a47ff484ea63a8f17987c
Francesco Ciocchetti <primeroznl@gmail.com>
2019-05-31 10:53:21 +0200

    add support dir

---
+++ support/vaultenv-podinfo/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+VAULT_TOKEN_FILE=${VAULT_TOKEN_FILE:-/home/vault/.vault-token}
+SECRETS_FILE=${SECRETS_FILE:-/secrets/secrets.file}
+
+# Expect token from vault-agent in "/home/vault/.vault-token"
+VAULT_TOKEN=$(cat $VAULT_TOKEN_FILE)
+
+exec /usr/bin/vaultenv \
+     --host ${VAULT_SERVICE_HOST} \
+     --port ${VAULT_SERVICE_PORT_VAULT} \
+     --token ${VAULT_TOKEN} \
+     --secrets-file ${SECRETS_FILE} \
+     /usr/bin/podinfo \
+     --port=8043
