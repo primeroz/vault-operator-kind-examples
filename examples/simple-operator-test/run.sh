cb92d0d84deb8131644ad07f07284ec7a28454a7
Francesco Ciocchetti <primeroznl@gmail.com>
2019-09-03 16:03:59 +0200

    Invert listener to match real config we use

--- examples/simple-operator-test/run.sh
+++ examples/simple-operator-test/run.sh
@@ -6,5 +6,4 @@ export BV_VERSION="0.4.18"
 ../../common/vault-operator.sh
 ../../common/vault-common.sh
 ./vault-cm-kunernetes-kv.sh
-../../common/vault-file-storage.sh
-
+./common/vault-file-storage.sh
--- examples/simple-operator-test/vault-file-storage.sh
+++ examples/simple-operator-test/vault-file-storage.sh
@@ -63,11 +63,11 @@ spec:
         path: "/vault/file"
     listener:
       - tcp:
-          address: "0.0.0.0:8200"
+          address: "0.0.0.0:8300"
           tls_cert_file: /vault/tls/server.crt
           tls_key_file: /vault/tls/server.key
       - tcp:
-          address: "0.0.0.0:8300"
+          address: "0.0.0.0:8200"
           tls_cert_file: /vault/tls/server.crt
           tls_key_file: /vault/tls/server.key
     telemetry:
