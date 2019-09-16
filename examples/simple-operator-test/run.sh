ac9e51d284006e2b0862c0a85d8b56d4b83f5d63
Francesco Ciocchetti <primeroznl@gmail.com>
2019-09-16 09:30:26 +0200

    no dex by default on this test

--- examples/simple-operator-test/run.sh
+++ examples/simple-operator-test/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-export BV_VERSION="0.4.18"
+export K8S_VERSION="v1.15.3@sha256:27e388752544890482a86b90d8ac50fcfa63a2e8656a96ec5337b902ec8e5157"
+export BV_VERSION="0.5.3"
+export VAULT_VERSION="1.2.2"
 ../../common/start-kind.sh
 ../../common/vault-operator.sh
 ../../common/vault-common.sh
-./run-dex.sh
+#./run-dex.sh
 ./vault-cm-kunernetes-kv.sh
 ./vault-file-storage.sh
